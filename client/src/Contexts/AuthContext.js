6d8a1496cf99b7e7504183850815222e97ef598e
keithwv <48747720+keithwv@users.noreply.github.com>
2022-01-19 12:22:40 -0700

    Create Authentication Context file

---
+++ client/src/Contexts/AuthContext.js
@@ -0,0 +1,32 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { auth } from '../Firebase/firebase-config';
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export default function AuthProvider(props) {
+    const [currentUser, setCurrentUser] = useState()
+
+    const register = (email, password) => {
+        auth.createUserWithEmailAndPassword(email, password) 
+    }
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user)
+        })
+        return unsubscribe
+    }, [])
+
+    const value = {
+        currentUser, 
+        register
+    }
+  return (
+      <AuthContext.Provider value={value}>
+          {props.children}
+      </AuthContext.Provider>
+}
