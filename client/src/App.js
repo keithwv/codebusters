db20f65d078c7a9fe6ca3f3dd046d7c508adacd3
Ali Ollaik <ollaikali@gmail.com>
2022-01-19 18:49:06 -0700

    Added Log In screen

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import theme from "./Components/User Interface/Theme";
 import { Route, Routes } from "react-router-dom";
 // import UserType from "./Components/User Type/UserType";
 import RegisterBusiness from "./Components/Register/RegisterBusiness";
+import SignIn from "./Components/Log In/loginBusiness";
 
 function App(props) {
   return (
@@ -14,7 +15,7 @@ function App(props) {
       <Routes>
         <Route path='/' element={<div>Home</div>} />
         <Route path='/Register' element={<RegisterBusiness />} />
-        <Route path='/Login' element={<div>Login</div>} />
+        <Route path='/Login' element={<SignIn />} />
         <Route path='/AboutUs' element={<div>About Us</div>} />
         <Route path='/ContactUs' element={<div>Contact us</div>} />
       </Routes>
