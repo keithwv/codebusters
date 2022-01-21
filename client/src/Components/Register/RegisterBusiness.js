aa6eb2f02fdd6faf286ae1e7e80df072e7c9be50
keithwv <48747720+keithwv@users.noreply.github.com>
2022-01-20 20:56:02 -0700

    Integrated react hook form into Register Business component

--- client/src/Components/Register/RegisterBusiness.js
+++ client/src/Components/Register/RegisterBusiness.js
@@ -1,54 +1,51 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { useForm, Controller } from "react-hook-form";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const theme = createTheme();
 const register = function () {
-  console.log("Registration completed")
-}
-
+  console.log("Registration completed");
+};
 
 export default function RegisterBusiness() {
-  const [selectBusiness, setSelectBusiness] = React.useState('');
+  const { control, handleSubmit, reset } = useForm();
 
-  const handleChange = (event) =>
-    setSelectBusiness(event.target.value);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
   };
 
   return (
@@ -58,92 +55,160 @@ export default function RegisterBusiness() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Register your Business
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit(onSubmit)}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
+                <Controller
                   name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
+                  defaultValue=""
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <TextField
+                      onChange={onChange}
+                      value={value}
+                      onBlur={onBlur}
+                      autoComplete="given-name"
+                      name="firstName"
+                      required
+                      fullWidth
+                      id="firstName"
+                      label="First Name"
+                      autoFocus
+                    />
+                  )}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
+                <Controller
                   name="lastName"
-                  autoComplete="family-name"
+                  defaultValue=""
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <TextField
+                      onChange={onChange}
+                      onBlur={onBlur}
+                      value={value}
+                      required
+                      fullWidth
+                      id="lastName"
+                      label="Last Name"
+                      name="lastName"
+                      autoComplete="family-name"
+                    />
+                  )}
                 />
               </Grid>
               <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
+                <Controller
                   name="email"
-                  autoComplete="email"
+                  defaultValue=""
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <TextField
+                      onChange={onChange}
+                      onBlur={onBlur}
+                      value={value}
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="email"
+                    />
+                  )}
                 />
               </Grid>
               <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
+                <Controller
                   name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
+                  defaultValue=""
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <TextField
+                      onChange={onChange}
+                      onBlur={onBlur}
+                      value={value}
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="new-password"
+                    />
+                  )}
                 />
               </Grid>
               <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
+                <Controller
                   name="password2"
-                  label="Confirm Your Password"
-                  type="password"
-                  id="password2"
-                  autoComplete="password2"
+                  defaultValue=""
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <TextField
+                      onChange={onChange}
+                      onBlur={onBlur}
+                      value={value}
+                      required
+                      fullWidth
+                      name="password2"
+                      label="Confirm Your Password"
+                      type="password"
+                      id="password2"
+                      autoComplete="password2"
+                    />
+                  )}
                 />
               </Grid>
 
-              <Grid id="select-business" item xs={12}>Select Business Category
+              <Grid id="select-business" item xs={12}>
+                Select Business Category
                 <FormControl xs={12} fullWidth>
-                  <Select
-                    labelId="select-business"
-                    id="deop-down-menu"
-                    value={selectBusiness}
-                    label="select-business"
-                    onChange={handleChange}
-                  >
-                    <MenuItem value={10}>General</MenuItem>
-                    <MenuItem value={20}>Car Repairs</MenuItem>
-                    <MenuItem value={30}>Cleaning Services</MenuItem>
-                  </Select>
+                  <Controller
+                    name="select-business"
+                    defaultValue=""
+                    control={control}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                      <Select
+                        onChange={onChange}
+                        onBlur={onBlur}
+                        value={value}
+                        labelId="select-business"
+                        id="drop-down-menu"
+                        label="select-business"
+                      >
+                        <MenuItem value="General">General</MenuItem>
+                        <MenuItem value="Car Repairs">Car Repairs</MenuItem>
+                        <MenuItem value="Cleaning Services">
+                          Cleaning Services
+                        </MenuItem>
+                      </Select>
+                    )}
+                  />
                 </FormControl>
               </Grid>
 
               <Grid item xs={12}>
                 <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
                   label="I want to receive inspiration, marketing promotions and updates via email."
                 />
               </Grid>
