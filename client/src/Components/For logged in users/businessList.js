b7b46a208992810aa958f6a9e527663d50564159
keithwv <48747720+keithwv@users.noreply.github.com>
2022-01-30 18:24:36 -0700

    Fixed map array in businessList

--- client/src/Components/For logged in users/businessList.js
+++ client/src/Components/For logged in users/businessList.js
@@ -1,59 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { db } from "../../Firebase/firebase-config";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 import { useAuth } from "../../contexts/AuthContext";
 import Card from "@mui/material/Card";
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
 import { makeStyles } from "@material-ui/styles";
 import { ListItemButton, ListItemText } from "@mui/material";
-import shortid from 'shortid';
+import shortid from "shortid";
 
 const Businesslist = () => {
   const { currentUser } = useAuth();
   const [business, setBusiness] = useState([]);
 
-  const getbusiness = async () => {
-    try {
-      let collectionRef = collection(db, "business");
-      let queryRef = query(collectionRef, where("uid", "==", currentUser.uid));
-      let querySnap = await getDocs(queryRef);
+  useEffect(() => {
+    let collectionRef = collection(db, "business");
+    let queryRef = query(collectionRef, where("uid", "==", currentUser.uid));
+    const unsubscribe = onSnapshot(queryRef, (querySnap) => {
       if (querySnap.empty) {
         console.log("No docs found");
       } else {
-        let businessData = querySnap.docs.map((doc) => ({
-          ...doc.data(),
-          DOC_ID: doc.id,
-        }));
+        let businessData = querySnap.docs.map((doc) => {
+          return {
+            ...doc.data(),
+            DOC_ID: doc.id,
+          };
+        });
         setBusiness(businessData);
       }
-    } catch (ex) {
-      console.log("FIRESTORE FAILURE!", ex.message);
-    }
-    console.log(business)
-  };
+    });
+    return unsubscribe;
+  }, []);
 
   return (
-      <>
-      <List>
-        <ListItemButton onClick={getbusiness}>Business Info</ListItemButton>
-          {business.map((business, index)=>(<ListItemText key={index} primary={`Company Name: ${business.company_name}`}/>))}
-          {business.map((business, index)=>(<ListItemText key={index} primary={`Name: ${business.name} ${business.last_name}`}/>))}
-          {business.map((business, index)=>(<ListItemText key={index} primary={`Street Address: ${business.address1} ${business.address2}`}/>))}
-          {business.map((business, index)=>(<ListItemText key={index} primary={`City: ${business.city}`}/>))}
-          {business.map((business, index)=>(<ListItemText key={index} primary={`Postal Code: ${business.postal_code}`}/>))}
-          {business.map((business, index)=>(<ListItemText key={index} primary={`Province: ${business.province}`}/>))}
-      </List>
-      </>
-    //   </CardContent>
-    // <div>
-    //   <button onClick={getbusiness}>Click Me</button>
-    //   {business.map((b) => (
-    //     <><div>{b.company_name}</div>
-    //     <div>{b.name}</div></>
-    //   ))}
-    // </div>
-    // </>
+    <>
+      {business.map((business) => {
+        return (
+          <List key={business.uid}>
+            <ListItemText primary={`Company Name: ${business.company_name}`} />
+            <ListItemText
+              primary={`Name: ${business.name} ${business.last_name}`}
+            />
+            <ListItemText
+              primary={`Street Address: ${business.address1} ${business.address2}`}
+            />
+            <ListItemText primary={`City: ${business.city}`} />
+            <ListItemText primary={`Postal Code: ${business.postal_code}`} />
+            <ListItemText primary={`Province: ${business.province}`} />
+          </List>
+        );
+      })}
+    </>
   );
 };
 
