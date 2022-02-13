3b101b607a1e5896408bcff598ae53586bbc9185
Ali Ollaik <ollaikali@gmail.com>
2022-02-12 23:51:42 -0700

    Businesses now shown when clicked on homepage imgs

--- client/src/Pages/list_of_businesses_categories.js
+++ client/src/Pages/list_of_businesses_categories.js
@@ -6,96 +6,77 @@ import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
-import { Button } from "@mui/material";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
-
-
+// import { Button } from "@mui/material";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { db } from "../Firebase/firebase-config";
 
 export default function ListOfBusinessesInCategory() {
-    
-    const [heroes, setHeroes] = React.useState([]);
-    const FirebaseContext = React.useContext(FirebaseContext);
-    const db = FirebaseContext.db;
-  
-    const AddBusinesses = async () => {
-      try {
-        let collectionRef = collection(db, "business");
-        let queryRef = query(collectionRef, orderBy("category"));
-        let querySnap = await getDocs(queryRef);
-        if (querySnap.empty) {
-          console.log("No docs found");
-        } else {
-          let heroesData = querySnap.docs.map((doc) => ({
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    let collectionRef = collection(db, "business");
+    let queryRef = query(collectionRef, orderBy("category"));
+    const undo = onSnapshot(queryRef, (querySnap) => {
+      if (querySnap.empty) {
+        console.log("No docs found");
+      } else {
+        let businessesList = querySnap.docs.map((doc) => {
+          return {
             ...doc.data(),
             DOC_ID: doc.id,
-          }));
-          setHeroes(heroesData);
-        }
-      } catch (ex) {
-        console.log("FIRESTORE FAILURE!", ex.message);
+          };
+        });
+        setList(businessesList);
       }
-    };
-
-    // const AddBusinesses = () => {
-    //     const list = useContext(FirebaseContext);
-    //     const db = list.db;
-    //     const [name, setName] = useState('');
-    //     const addHero = async (heroName, heroVehicle) => {
-    //       try {
-    //         let collectionRef = collection(db, 'business');
-    //         await addDoc(collectionRef, { name: heroName, vehicle: heroVehicle });
-    //         setName('');
-    //         setVehicle('');
-    //       } catch (ex) {
-    //         console.log('FAILED TO LOAD THE LIST', ex.message);
-    //       }
-    //     }
-    // }
-//   const FirebaseContext = React.createContext();
-
-//   const FirebaseProvider = (props) => {
-//     const children = props.children;
-//     const theValues = { app };
-//     return (
-//       <FirebaseContext.Provider value={theValues}>
-//         {children}
-//       </FirebaseContext.Provider>
-//     );
-//   };
+    });
+    return undo;
+  }, []);
 
   return (
-    <><Button
-          sx={{ minWidth: "135px", height: "40px" }}
-          variant="contained"
+    <>
+      <List
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "background.paper",
+          ml: "240px",
+        }}
       >
-          Some button
-      </Button><List
-          sx={{
-              width: "100%",
-              maxWidth: 360,
-              bgcolor: "background.paper",
-              ml: "240px",
-          }}
-      >
-              <ListItem alignItems="flex-start">
-                  <ListItemAvatar>
-                      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-                  </ListItemAvatar>
-                  <ListItemText
-                      primary="Brunch this weekend?"
-                      secondary={<React.Fragment>
-                          <Typography
-                              sx={{ display: "inline" }}
-                              component="span"
-                              variant="body2"
-                              color="text.primary"
-                          >
-                              Ali Connors
-                          </Typography>
-                          {" — I'll be in your neighborhood doing errands this…"}
-                      </React.Fragment>} />
-              </ListItem>
-              <Divider variant="inset" component="li" />
-          </List></>
+        {list.map((item) => {
+          return (
+            <ul key={item.DOC_ID}>
+              <li>category: {item.category}</li>
+              <li>docId: {item.DOC_ID}</li>
+              <hr />
+            </ul>
+          );
+        })}
+        <br />
+
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Brunch this weekend?"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Ali Connors
+                </Typography>
+                {" — I'll be in your neighborhood doing errands this…"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+      </List>
+    </>
   );
 }
