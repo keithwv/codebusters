f3ef47661cc6f9ff875c8f439b1b781bb77c802c
Ali Ollaik <ollaikali@gmail.com>
2022-03-01 17:45:18 -0700

    fixed a bug

--- client/src/Components/ProtectedRoute.js
+++ client/src/Components/ProtectedRoute.js
@@ -2,17 +2,16 @@ import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
-
 const ProtectedRoute = (props) => {
   const { children } = props;
   const { currentUser } = useAuth();
   const location = useLocation();
-  let output =
-    currentUser === true ? (
-      children
-    ) : (
-      <Navigate to="/login" replace state={{ path: location.pathname }} />
-    );
+  let output = currentUser ? (
+    children
+  ) : (
+    <Navigate to="/login" replace state={{ path: location.pathname }} />
+  );
+
   return output;
 };
 
